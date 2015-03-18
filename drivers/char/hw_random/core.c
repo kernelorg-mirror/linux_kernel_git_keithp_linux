707fe18873ddfe9c06c88fa33e24296050139a0a
Keith Packard <keithp@keithp.com>
2015-03-17 22:01:03 -0700

    hwrng: core - allow perfect entropy from hardware devices
    Hardware random number quality is measured from 0 (no entropy) to 1024
    (perfect entropy). Allow hardware devices to assert the full range by
    truncating the device-provided value at 1024 instead of 1023.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- drivers/char/hw_random/core.c
+++ drivers/char/hw_random/core.c
@@ -98,7 +98,8 @@ static inline int hwrng_init(struct hwrng *rng)
 	add_early_randomness(rng);
 
 	current_quality = rng->quality ? : default_quality;
-	current_quality &= 1023;
+	if (current_quality > 1024)
+		current_quality = 1024;
 
 	if (current_quality == 0 && hwrng_fill)
 		kthread_stop(hwrng_fill);
